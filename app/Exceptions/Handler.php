ff59e80b3a0462f9e381132ee1093180ff1b44db
Tariq Ayman <tariq.ayman94@gmail.com>
2021-04-29 06:39:35 +0200

    add unauthenticated method in Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -51,5 +52,29 @@ class Handler extends ExceptionHandler
     public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+
+        if ($request->expectsJson() || $request->route()->getPrefix() == 'api') {
+            return response()->json(['errors' => ['Unauthenticated.']], 401);
+        }
+
+        $guard = $exception->guards()[0] ?? null;
+
+        switch ($guard) {
+            case 'admin':
+                $login = 'admin.login';
+                break;
+            case 'user':
+                $login = 'user.login';
+                break;
+            default:
+                $login = 'login';
+                break;
+        }
+
+        return redirect()->guest(route($login));
     }
 }
