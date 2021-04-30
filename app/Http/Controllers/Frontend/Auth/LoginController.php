c0991ccf419f02b5941cd9acb91b5c2ca9095e4d
Tariq Ayman <tariq.ayman94@gmail.com>
2021-04-30 07:04:03 +0200

    create auth controller for frontend

---
+++ app/Http/Controllers/Frontend/Auth/LoginController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use anlutro\LaravelSettings\Facade as Setting;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+    protected $maxAttempts;
+    protected $decayMinutes;
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+        $this->middleware('auth:user', ['only' => ['getLogout']]);
+        $this->maxAttempts = Setting::get('max_login_attempts', 3);
+        $this->decayMinutes = Setting::get('lockout_delay', 2);
+    }
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\RedirectResponse
+     */
+    public function showLoginForm()
+    {
+        if (Auth::guard('user')->check()) {
+            return redirect()->route('frontend.profile.index');
+        }
+
+        return view('frontend.user.login');
+    }
+
+    public function login(Request $request)
+    {
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            $this->sendLockoutResponse($request);
+            return redirect()->route('user.login')->withInput()->withwarning('You are locked! Too many attempts. please try ' . setting('lockout_delay') . ' minutes later.');
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::guard('user')->attempt(['email' => $credentials['email'], 'password' => $credentials['password']], $request->remember)) {
+            $userStatus = Auth::guard('user')->user()->status;
+
+//            if ($userStatus == 1) {
+                return redirect()->intended(route('frontend.profile.index'));
+//            } else {
+//                Auth::guard('user')->logout();
+//                return redirect()->route('login')->withInput()->withwarning('You are temporary blocked. please contact to admin');
+//            }
+        } else {
+            $this->incrementLoginAttempts($request);
+            return redirect()->route('login')->withInput()->withErrors('Incorrect username or password. Please try again');
+        }
+
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @return RedirectResponse
+     */
+    public function getLogout()
+    {
+        \Auth::logout();
+        \Auth::guard('user')->logout();
+        return redirect()->route('login');
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @return RedirectResponse
+     */
+    public function logout()
+    {
+        \Auth::guard('user')->logout();
+        return redirect()->route('login');
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+}
---
+++ app/Http/Controllers/Frontend/Auth/RegisterController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use App\Models\Admin;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+use App\Mail\UserRegistered;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = 'login';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param array $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'phone_number' => ['required', 'string', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param array $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data)
+    {
+        $user = User::create($data);
+
+        if (setting('register_notification_email')) {
+            Mail::to($data['email'])->send(new UserRegistered($user));
+        }
+
+        return $user;
+    }
+
+    /**
+     * Show the application registration form.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showRegistrationForm()
+    {
+        if (Auth::guard('user')->check()) {
+            return redirect()->route('frontend.profile.index');
+        }
+
+        return view('frontend.user.register');
+    }
+
+    /**
+     * Get the guard to be used during registration.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+}
