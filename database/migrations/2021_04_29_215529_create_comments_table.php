ae2dee66f2edc190a6a8a47a971095b76e3e27bf
Tariq Ayman <tariq.ayman94@gmail.com>
2021-04-30 07:00:29 +0200

    create comment table migration

---
+++ database/migrations/2021_04_29_215529_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('auction_id');
+            $table->foreign('auction_id')->references('id')->on('auctions')->onDelete('cascade');
+            $table->text('comment');
+            $table->boolean('status')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
