f32832beb41491c02336c491baf765d4d213bcd2
Tariq Ayman <tariq.ayman94@gmail.com>
2021-05-01 07:45:40 +0200

    allow save images as array

--- app/Helpers/UploadFile.php
+++ app/Helpers/UploadFile.php
@@ -4,19 +4,33 @@
 namespace App\Helpers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 
 trait UploadFile
 {
     /**
      * @param Request $request
-     * @param $attribute
-     * @param $path
-     * @param string $diskName
+     * @param string $attribute
+     * @param string $path
+     * @param int $indexArray
      * @return string
      */
-    public function uploadFile(Request $request, $attribute, $path, $diskName = 'local'): ?string
+    public function uploadFile(Request $request, string $attribute, string $path, int $indexArray = 0): ?string
     {
+        if (is_array($request->$attribute)) {
+
+            $file = $request->$attribute[$indexArray];
+
+            if ($file instanceof UploadedFile) {
+                $filename = time() . md5(time() . $file->getClientOriginalName()) . ".{$file->getClientOriginalExtension()}";
+
+                $file->move(public_path("storage/{$path}/"), $filename);
+
+                return "storage/{$path}/{$filename}";
+            }
+        }
+
         if ($uploadedFile = $request->file($attribute)) {
 
             $filename = time() . md5(time() . $uploadedFile->getClientOriginalName()) . ".{$uploadedFile->getClientOriginalExtension()}";
