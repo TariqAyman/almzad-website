4ec57ff14ebe506c58d326395c6e04952227266d
Tariq Ayman <tariq.ayman94@gmail.com>
2021-05-01 07:51:35 +0200

    enhance AuctionUser Requests

--- app/Http/Requests/Auction/AuctionStoreRequest.php
+++ app/Http/Requests/Auction/AuctionStoreRequest.php
@@ -47,7 +47,7 @@ class AuctionStoreRequest extends FormRequest
             'start_date' => 'required|date',
             'end_date' => 'required|date',
             'shipping' => 'required|boolean',
-            'shippingFree' => 'required|boolean',
+            'shipping_free' => 'required|boolean',
             'multi_auction' => 'required|boolean',
         ];
     }
