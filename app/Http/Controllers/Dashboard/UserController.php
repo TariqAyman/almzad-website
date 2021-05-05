ee8d5f889e1b0c57a9d0173c468fae773b0b4da2
Tariq Ayman <tariq.ayman94@gmail.com>
2021-05-05 02:53:20 +0200

    create users CRUD

---
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Helpers\UploadFile;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ADminStoreRequest;
+use App\Http\Requests\AdminUpdateRequest;
+use App\Http\Requests\User\UserStoreRequest;
+use App\Http\Requests\User\UserUpdateRequest;
+use App\Models\Admin;
+use App\Models\User;
+use DataTables;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
+
+class UserController extends Controller
+{
+    use UploadFile;
+
+    public function __construct()
+    {
+        $this->middleware('permission:view-user');
+        $this->middleware('permission:create-user', ['only' => ['create', 'store']]);
+        $this->middleware('permission:update-user', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:destroy-user', ['only' => ['destroy']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('dashboard.user.index');
+    }
+
+    public function getDatatable()
+    {
+        return DataTables::of(User::orderBy('updated_at', 'desc')->get())
+            ->addIndexColumn()
+            ->addColumn('action', function ($model) {
+                $edit = '<a href="' . route('admin.user.edit', $model->id) . '" type="button" class="btn btn-sm btn-info">' . trans('app.edit') . '</a> ';
+                $delete = '<button id="deleteButton" data-id="' . $model->id . '" type="button" class="btn btn-sm btn-danger">' . trans('app.delete') . '</button>';
+                return $edit . ' ' . $delete;
+            })
+            ->addColumn('profile_photo', function ($model) {
+                $profile_photo = asset($model->profile_photo);
+                return $model->profile_photo ? "<img height='100px' alt='Image placeholder' class='avatar avatar-sm rounded-circle' data-toggle='tooltip' data-original-title='{$model->name}' src='{$profile_photo}'>" :
+                    '<i class="far avatar avatar-sm rounded-circle fa-user"></i>';
+            })
+            ->addColumn('status', function ($model) {
+                return $model->status ? '<span class="badge badge-pill badge-lg badge-success">Active</span>' : '<span class="badge badge-pill badge-lg badge-danger">Disabled</span>';
+            })
+            ->addColumn('email_verified_at', function ($model) {
+                return $model->email_verified_at ? $model->email_verified_at->diffForHumans() : trans('app.email_verified_at_yet');
+            })
+            ->addColumn('created_at', function ($model) {
+                return $model->created_at->diffForHumans();
+            })
+            ->addColumn('updated_at', function ($model) {
+                return $model->updated_at->diffForHumans();
+            })
+            ->rawColumns(['action', 'created_at', 'updated_at', 'status', 'profile_photo'])
+            ->make(true);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $edit = false;
+
+        return view('dashboard.user.form', compact( 'edit'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param ADminStoreRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserStoreRequest $request)
+    {
+        $userData = $request->except(['role', 'profile_photo']);
+
+        if ($request->profile_photo) {
+            $userData['profile_photo'] = $this->uploadFile($request, 'profile_photo', 'admins');
+        }
+
+        $user = User::create($userData);
+
+        return redirect()->route('admin.user.index')->withsuccess('User created successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param User $user
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return view('dashboard.users.show', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param User $user
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        $edit = true;
+
+        return view('dashboard.user.form', compact('user',  'edit'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param AdminUpdateRequest $request
+     * @param User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UserUpdateRequest $request, User $user)
+    {
+        $userData = $request->except(['role', 'profile_photo']);
+
+        if ($request->profile_photo) {
+            $userData['profile_photo'] = $this->uploadFile($request, 'profile_photo', 'admins');
+        }
+
+        $user->update($userData);
+
+        return redirect()->route('admin.user.index')->withSuccess('User updated successfully!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Request $request)
+    {
+        $user = User::find($request->id);
+
+        if ($request->id == Auth::user()->id) {
+            return response()->json(['message' => 'You can not delete logged in user!']);
+        }
+
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully!']);
+    }
+}
---
+++ resources/views/dashboard/user/form.blade.php
@@ -0,0 +1,140 @@
+@extends('dashboard.layouts.contentLayoutMaster')
+
+@if($edit)
+    @section('page-title', trans('app.edit_users'))
+@section('page-heading', trans('app.edit_users'))
+@else
+    @section('page-title', trans('app.add_user'))
+@section('page-heading', trans('app.add_user'))
+@endif
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mb-5">
+                <div class="card-body">
+                    @if($edit)
+                        {!! Form::open(['route' => ['admin.user.update',$user->id], 'method' => 'PUT','files' => true, 'id' => 'update-form']) !!}
+                    @else
+                        {!! Form::open(['route' => 'admin.user.store', 'files' => true, 'id' => 'store-form']) !!}
+                    @endif
+                    <h6 class="heading-small text-muted mb-4">@lang('app.User information')</h6>
+                    <div class="pl-lg-4">
+                        <div class="form-group row">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    {{ Form::label('first_name', trans('app.first_name'), ['class' => 'form-control-label']) }}
+                                    {{ Form::text('first_name', $edit ? $user->first_name : old('first_name'), ['class' => 'form-control']) }}
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    {{ Form::label('last_name', trans('app.last_name'), ['class' => 'form-control-label']) }}
+                                    {{ Form::text('last_name', $edit ? $user->last_name : old('last_name'), ['class' => 'form-control']) }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    {{ Form::label('email', trans('app.email'), ['class' => 'form-control-label']) }}
+                                    {{ Form::email('email', $edit ? $user->email : old('email'), ['class' => 'form-control']) }}
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    {{ Form::label('username', trans('app.username'), ['class' => 'form-control-label']) }}
+                                    {{ Form::text('username', $edit ? $user->username : old('username'), ['class' => 'form-control']) }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    {{ Form::label('phone_number', trans('app.phone_number'), ['class' => 'form-control-label']) }}
+                                    {{ Form::tel('phone_number', $edit ? $user->phone_number : old('phone_number'), ['class' => 'form-control']) }}
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    {{ Form::label('address', trans('app.address'), ['class' => 'form-control-label']) }}
+                                    {{ Form::text('address', $edit ? $user->address : old('address'), ['class' => 'form-control']) }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    {{ Form::label('profile_photo', trans('app.Photo'), ['class' => 'form-control-label d-block']) }}
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="customFile" name="profile_photo"/>
+                                        <label class="custom-file-label" for="customFile">Choose Photo</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                @if ($edit && $user->profile_photo)
+                                    <a href="{{ asset($user->profile_photo) }}" target="_blank">
+                                        <img alt="Image placeholder"
+                                             class="avatar avatar-xl  rounded-circle"
+                                             data-toggle="tooltip" data-original-title="{{ $user->name }} Logo"
+                                             src="{{ asset($user->profile_photo) }}">
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                        <hr class="my-4"/>
+                        <!-- Address -->
+                        <h6 class="heading-small text-muted mb-4">@lang('app.Password information')</h6>
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    {{ Form::label('password', trans('app.Password'), ['class' => 'form-control-label']) }}
+                                    {{ Form::password('password', ['class' => 'form-control']) }}
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    {{ Form::label('password_confirmation', trans('app.Confirm password'), ['class' => 'form-control-label']) }}
+                                    {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr class="my-4"/>
+                        <div class="pl-lg-4">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="custom-control custom-checkbox">
+                                        {!! Form::hidden('status', 0) !!}
+                                        <input type="checkbox" name="status" value="1" {{ ($edit && $user->status) ? 'checked' : ''}} class="custom-control-input" id="status">
+                                        {{ Form::label('status', 'Status', ['class' => 'custom-control-label']) }}
+                                    </div>
+                                </div>
+                                @can('update-user')
+                                    <div class="col-md-12">
+                                        {{ Form::submit('Submit', ['class'=> 'mt-5 btn btn-primary']) }}
+                                    </div>
+                                @endcan
+                            </div>
+                        </div>
+                    </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script src="{{ asset('vendor/laravel-filemanager/js/stand-alone-button.js') }}"></script>
+    <script>
+        jQuery(document).ready(function () {
+            jQuery('#uploadFile').filemanager('file');
+        })
+    </script>
+@endpush
