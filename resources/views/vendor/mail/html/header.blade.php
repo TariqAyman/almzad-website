5c50535f90469e7afae344c3ad2d3e4fb7451200
Tariq Ayman <tariq.ayman94@gmail.com>
2021-05-11 03:36:59 +0200

    add mail vendor views

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,17 @@
+<tr>
+    <td class="header">
+        @if(setting('company_name'))
+            <a href="{{ env('APP_URL') }}" style="display: inline-block;">
+                <img src="{{ setting('company_logo') ? asset(setting('company_logo')) : asset('frontend/img/logo.png') }}" alt="{{ setting('company_name') }}" class="logo">
+            </a>
+        @else
+            <a href="{{ $url }}" style="display: inline-block;">
+                @if (trim($slot) === 'Laravel')
+                    <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+                @else
+                    {{ $slot }}
+                @endif
+            </a>
+        @endif
+    </td>
+</tr>
